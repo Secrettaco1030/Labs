35cff9e2ea582b8e61aef19b867c4c276b3fcf92
Secrettaco1030 <1agarwalron@hdsb.ca>
2025-12-09 10:35:56 -0500

    Plaid is done

--- Pygame Challenge 01 - Plaid.py
+++ Pygame Challenge 01 - Plaid.py
@@ -1,28 +1,35 @@
 import pygame
 
 pygame.init()
+stripe1=(243,243,243)
+#Whtie
+stripe2=((37,0,1))
+#Mahogany
+stripe3=(165,1,4)
+#Inferno
+stripe4=(252,186,4)
+#Amber Flame
+stripe5=(115,44,44)
 
-stripe1=(255,238,169)
-#light yellow
-stripe2=((255,190,120))
-#light orange
-stripe3=(255,125,41)
-#orange
-stripe4=(37,57,0)
-#brown
 width=600
 height=400
 screen = pygame.display.set_mode((600, 400))
 pygame.display.set_caption("Pygame Test Window")
 stripe_width=45
+stripe_width2=10
+stripe_height=45
+stripe_height2=10
 gap=80
 
-screen.fill(stripe4)
+screen.fill(stripe2)
 
 for x in range(0, width, gap):
-    pygame.draw.rect(screen, stripe2, (x,0, stripe_width, height))
+    pygame.draw.rect(screen, stripe1, (x,0, stripe_width, height))
 
-
+for y in range(0, height, gap):
+    pygame.draw.rect(screen, stripe4, (0,y, width, stripe_height))
+for z in range(0, height, gap):
+    pygame.draw.rect(screen, stripe3, (0,z, width, stripe_height2))
 
 
 running = True
