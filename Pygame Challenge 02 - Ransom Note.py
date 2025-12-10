5bafda7c42725c310609fc95364578f4c5ae601f
Secrettaco1030 <1agarwalron@hdsb.ca>
2025-12-10 11:03:13 -0500

    Image Collage
    might add a sun at the top as the random element

--- Pygame Challenge 02 - Ransom Note.py
+++ Pygame Challenge 02 - Ransom Note.py
@@ -1,2 +1,11 @@
+import pygame
+pygame.init()
+window = pygame.display.set_mode((800, 600))
+window.fill((186, 217, 181))
 
+font = pygame.font.SysFont("Arial", 20)
+renderedText = font.render("Hello World!", True, (89, 0, 0))
+window.blit(renderedText, (50, 50))
+
+pygame.display.flip()
 
--- Pygame Challenge 03 - Image Collage.py
+++ Pygame Challenge 03 - Image Collage.py
@@ -1,33 +1,26 @@
-# -----------------------------------------------------------------------------
-# IMAGE LAB - IMAGE COLLAGE
-# 
-# Use your new knowledge of drawing and colours with Pygame to make this a full screen of a plaid pattern.
-# 
-# Lab Requirements:
-# LEVEL 3
-# Use at least 3 different images to create a collage
-# You can repeat the same images must use other commands to change the look (example, tint)
+import pygame
+pygame.init()
 
-# LEVEL 4
-# Everything listed in level 3 
-# Create a scene with images
-
-# LEVEL 4+
-# Everything listed in level 4
-# Randomize or animate something
-
-# Recommended Lessons:
-# P5.js intro
-# P5.js drawing
-# P5.js colour
-# 
-# Challenge Difficulty:**
-# 
-# Remember the purpose of this challenge is to help you practice Pygame coding not to find code online or copy from your friends! This challenge will be checked for Plagiarism.
-# 
-# Upload your code to githun when finished
-# 
-# Good luck!
-#-----------------------------------------------------------------------------
-
-
+screen = pygame.display.set_mode((800, 600))
+background = pygame.image.load('/Users/ronnie/Documents/GitHub/Labs/images/park.jpg')
+background = pygame.transform.scale(background, (800, 600))
+screen.blit(background, (0, 0))
+pygame.display.update()
+car1 = pygame.image.load('/Users/ronnie/Documents/GitHub/Labs/images/car2.png')
+car1 = pygame.transform.scale(car1, (400, 340))
+screen.blit(car1, (200, 230))
+pygame.display.update()
+car2=pygame.image.load("/Users/ronnie/Documents/GitHub/Labs/images/car3.png")
+car2 = pygame.transform.scale(car2, (350, 340))
+screen.blit(car2, (500, 250))
+pygame.display.update()
+car4=pygame.image.load("/Users/ronnie/Documents/GitHub/Labs/images/car4.png")
+car4 = pygame.transform.scale(car4, (300, 340))
+screen.blit(car4, (-30, 250))
+pygame.display.update()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
