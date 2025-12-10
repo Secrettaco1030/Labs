74e904ae388ba5a0fc28754783d915ca4acfe138
Secrettaco1030 <1agarwalron@hdsb.ca>
2025-12-10 17:52:19 -0500

    Completed Image Collage
    the sun is the random element

--- Pygame Challenge 03 - Image Collage.py
+++ Pygame Challenge 03 - Image Collage.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
@@ -15,8 +16,14 @@ car2 = pygame.transform.scale(car2, (350, 340))
 screen.blit(car2, (500, 250))
 pygame.display.update()
 car4=pygame.image.load("/Users/ronnie/Documents/GitHub/Labs/images/car4.png")
-car4 = pygame.transform.scale(car4, (300, 340))
-screen.blit(car4, (-30, 250))
+car4 = pygame.transform.scale(car4, (330, 340))
+screen.blit(car4, (-20, 230))
+sun_x=random.randrange(40,650)
+sun1=pygame.image.load("/Users/ronnie/Documents/GitHub/Labs/images/sun2.png")
+sun1 = pygame.transform.scale(sun1, (150, 150))
+screen.blit(sun1, (sun_x, 0))
+pygame.display.update()
+
 pygame.display.update()
 running = True
 while running:
