df910e3a917b3591e241bd831f5fb4ca15e3f3c7
Secrettaco1030 <1agarwalron@hdsb.ca>
2025-12-18 10:39:33 -0500

    Menu

--- Pygame Challenge 05 - Menu.py
+++ Pygame Challenge 05 - Menu.py
@@ -1,49 +1,82 @@
-# -----------------------------------------------------------------------------
-# GAMESTATE LAB - MENU
-# 
-# Use your new knowledge of drawing and colours with Pygame to make this a full screen of a plaid pattern.
-# 
-# Lab Requirements:
-# LEVEL 3
-# A minimum of 3 possible unique backdrops
-# A minimum of 2 buttons that when clicked will change the view of the user
-# A method to return to the main menu
-# HTML Buttons
-# Uses gamestates
-# LEVEL 4
-# A minimum of 4 possible unique windows
-# A minimum of 3 buttons that when clicked will change the backdrop or view of the user
-# A method to return to the main menu
-# Use gamestates
-# Collision detection style buttons
-# LEVEL 4+
-# Everything listed in level 3 and 4
-# Add sound effects when the button is pressed or add a mouseover effect to the button (the button highlights or changes looks when the mouse goes over the locations of the button)
-# Recommended Lessons:
-# Recommended Lessons:
-# Github
-# Thonny
-# Pygame Intro
-# Pygame Window
-# Pygame Game Loop
-# Pygame Drawing
-# Pygame Colours
-# Pygame Images
-# Pygame Animation
-# Pygame Sounds
-# Pygame The Game Loop
-# Pygame Events
-# Pygame Gamestates
-# Pygame Collision Detection
-# Pygame Sprites
-# 
-# Challenge Difficulty:****
-# 
-# Remember the purpose of this challenge is to help you practice Pygame coding not to find code online or copy from your friends! This challenge will be checked for Plagiarism.
-# 
-# Upload your code to github when finished
-# 
-# Good luck!
-#-----------------------------------------------------------------------------
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+clock=pygame.time.Clock()
+
+gameState="Menu"
+font=pygame.font.SysFont("Times New Roman", 30)
+
+White=(255,255,255)
+Black=(0,0,0)
+Grey=(200,200,200)
+Dark_Grey=(100,100,100)
+Blue=(0,0,255)
+Green=(0,255,0)
+Pink=(255,0,255)
+green_button=pygame.Rect(150,150,200,50)
+pink_button=pygame.Rect(150,230,200,50)
+black_button=pygame.Rect(150,300,200,50)
+back_button=pygame.Rect(510,500,270,50)
 
 
+running=True
+while running:
+    mousepressed=False
+    mouseX, mouseY = pygame.mouse.get_pos()
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            running = False
+        if ev.type == pygame.MOUSEBUTTONUP:
+            mousepressed=True
+
+
+    if gameState=="Menu":
+        screen.fill(Blue)
+        green_colour= Dark_Grey if green_button.collidepoint(mouseX,mouseY) else Green
+        pink_colour= Dark_Grey if pink_button.collidepoint(mouseX,mouseY) else Pink
+        black_colour= Dark_Grey if black_button.collidepoint(mouseX,mouseY) else Black
+        pygame.draw.rect(screen,pink_colour,pink_button)
+        pygame.draw.rect(screen,green_colour,green_button)
+        pygame.draw.rect(screen,black_colour,black_button)
+        screen.blit(font.render("Main Menu", True, Black),(120,50))
+        screen.blit(font.render("Credits", True, Black),(160,160))
+        screen.blit(font.render("Settings", True, Black),(160,240))
+        screen.blit(font.render("About", True, White),(160,310))
+
+        if mousepressed:
+            if green_button.collidepoint(mouseX,mouseY):
+                gameState="Credits"
+            elif pink_button.collidepoint(mouseX,mouseY):
+                gameState="Settings"
+            elif black_button.collidepoint(mouseX,mouseY):
+                gameState="About"
+    elif gameState=="Credits":
+        screen.fill(Green)
+        back_colour= Dark_Grey if back_button.collidepoint(mouseX,mouseY) else Grey
+        pygame.draw.rect(screen,back_colour,back_button)
+        screen.blit(font.render("Back to Main Menu", True, Black),(525,510))
+        if mousepressed and back_button.collidepoint(mouseX,mouseY):
+            gameState="Menu"
+    elif gameState=="Settings":
+        screen.fill(Pink)
+        back_colour= Dark_Grey if back_button.collidepoint(mouseX,mouseY) else Grey
+        pygame.draw.rect(screen,back_colour,back_button)
+        screen.blit(font.render("Back to Main Menu", True, Black),(525,510))
+        if mousepressed and back_button.collidepoint(mouseX,mouseY):
+            gameState="Menu"
+    elif gameState=="About":
+        screen.fill(Black)
+        back_colour= Dark_Grey if back_button.collidepoint(mouseX,mouseY) else Grey
+        pygame.draw.rect(screen,back_colour,back_button)
+        screen.blit(font.render("Back to Main Menu", True, White),(525,510))
+        if mousepressed and back_button.collidepoint(mouseX,mouseY):
+            gameState="Menu"
+
+
+
+
+
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
